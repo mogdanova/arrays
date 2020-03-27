60d6dcacafaa5980685cdf24d327e6b094084cac
mogdanova <56578892+mogdanova@users.noreply.github.com>
2020-03-27 08:05:40 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+var start = document.getElementById("start");
+var stop = document.getElementById("stop");
+var step = document.getElementById("step");
+console.log(start);
+console.log(stop);
+console.log(step);
+
 function initial(m, k){
   m = [1,2,3,4,5,6,7,8,9,0];
   k = 4;
@@ -11,11 +18,28 @@ function compact(arr) {
   return onlyTruthyValues;
 }
 
-function union(value, index, self){
-  self = [1, 2, 3], [1, 5, 3, 6], [1, 5, 4], [2, 7, 1];
-  return self.indexOf(value) === index;
+function union(arr){
+  arr = [1, 2, 3, 1, 5, 3, 6, 1, 5, 4, 2, 7, 1];
+  let result = [];
+  for (let str of arr) {
+    if (!result.includes(str)) {
+      result.push(str);
+    }
+  }
+  return result;  
 }
 
-function range(){
-
+function range(a, b, c){
+  let result = [];
+  if(b == null){
+    for (var i = 0; i < a; i+=c) {
+      result.push(i);
+    }
+  }
+  else {
+    for (var i = a; i < b; i+=c) {
+      result.push(i);
+    }
+  }
+  return result;
 }
